816a0ef4aa36e90d10649ca5d9b1eab664fde867
Davis Vielma <davisvielma@gmail.com>
2024-08-02 10:32:01 -0400

    Creacion de EV con su archivo requirements.txt e instalacion de colorama

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from colorama import Fore, Style
+
 def main() -> None:
-	print('Hola Mundo!!!')
+	print(Fore.WHITE + 'Pueba con color blanco')
+	print(Fore.BLUE + f'Pueba con color azul al numero {35}')
+	print(Style.RESET_ALL + 'Reestablecido el estilode letra or defecto')
 
 if __name__ == '__main__':
 	main()
